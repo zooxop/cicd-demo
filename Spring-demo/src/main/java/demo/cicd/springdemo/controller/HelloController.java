464bd1fae62b44e8188feca2381527d58b1a3faf
Cheolhyun Mun <zooxop@gmail.com>
2025-01-13 04:28:55 +0900

    feat: HelloController 추가

---
+++ Spring-demo/src/main/java/demo/cicd/springdemo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package demo.cicd.springdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "<h1>Hello</h1>";
+    }
+}
