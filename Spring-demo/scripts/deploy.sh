4bff6e0647f5b6373a71eb75ee3228da77792841
Cheolhyun Mun <zooxop@gmail.com>
2025-01-13 18:19:17 +0900

    fix: deploy script for macOS

--- Spring-demo/scripts/deploy.sh
+++ Spring-demo/scripts/deploy.sh
@@ -1,58 +1,46 @@
-#!/bin/bash
-# 작업 디렉토리를 /var/jenkins_home/custom/cicd-demo으로 변경
-cd /var/jenkins_home/custom/cicd-demo
+#!/bin/zsh
+# 작업 디렉토리 변경
+cd /Users/$(whoami)/jenkins_home/custom/cicd-demo
 
-# 환경변수 DOCKER_APP_NAME : 컨테이너 메인 이름
+# 환경변수 설정
 DOCKER_APP_NAME=spring-cicd-demo
 LOG_FILE=./deploy.log
 
-# 실행중인 blue가 있는지 확인
-# 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지 여부를 EXIST_BLUE 변수에 저장
-EXIST_BLUE=$(docker-compose -p "${DOCKER_APP_NAME}-blue" -f docker-compose.blue.yml ps | grep -E "Up|running")
+# MacOS의 grep 명령어는 다르게 동작하므로 수정
+EXIST_BLUE=$(docker compose -p "${DOCKER_APP_NAME}-blue" -f docker-compose.blue.yml ps | grep -E "Up|running")
 
-# 배포 시작한 날짜와 시간을 기록
-echo "배포 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
+# MacOS의 date 포맷 수정
+echo "배포 시작일자 : $(date '+%Y-%m-%d %H:%M:%S')" >> $LOG_FILE
 
-
-# green이 실행중이면 blue up
-# EXIST_BLUE 변수가 비어있는지 확인
 if [ -z "$EXIST_BLUE" ]; then
+  echo "blue 배포 시작 : $(date '+%Y-%m-%d %H:%M:%S')" >> $LOG_FILE
 
-  # 로그 파일에 "blue up - blue 배포 : port:8081"이라는 내용을 추가
-  echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
+  # docker compose 명령어 수정 (하이픈 제거)
+  docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
 
-  # docker-compose.blue.yml 파일을 사용하여 blue 컨테이너를 빌드하고 실행
-  docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
-
-  # 30초 동안 대기
   sleep 30
 
-  # 로그 파일에 "green 중단 시작"이라는 내용을 추가
-  echo "green 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
+  echo "green 중단 시작 : $(date '+%Y-%m-%d %H:%M:%S')" >> $LOG_FILE
+  docker compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 
-  # docker-compose.green.yml 파일을 사용하여 green 컨테이너를 중지
-  docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
-
-   # 사용하지 않는 이미지 삭제
   docker image prune -af
 
-  echo "green 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
+  echo "green 중단 완료 : $(date '+%Y-%m-%d %H:%M:%S')" >> $LOG_FILE
 
-# blue가 실행중이면 green up
 else
-  echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
-  docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+  echo "green 배포 시작 : $(date '+%Y-%m-%d %H:%M:%S')" >> $LOG_FILE
+  docker compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
 
   sleep 30
 
-  echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
-  docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+  echo "blue 중단 시작 : $(date '+%Y-%m-%d %H:%M:%S')" >> $LOG_FILE
+  docker compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
   docker image prune -af
 
-  echo "blue 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
+  echo "blue 중단 완료 : $(date '+%Y-%m-%d %H:%M:%S')" >> $LOG_FILE
 
 fi
-  echo "배포 종료  : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> $LOG_FILE
+  echo "배포 종료  : $(date '+%Y-%m-%d %H:%M:%S')" >> $LOG_FILE
 
   echo "===================== 배포 완료 =====================" >> $LOG_FILE
   echo >> $LOG_FILE
